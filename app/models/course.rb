9360d81db2776177951227d9a4e7d3f145313006
Brian Randall <brian.lewis.randall@outlook.com>
2019-10-30 01:32:40 +0000

    flixter

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :titles, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
