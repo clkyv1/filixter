a13c9c12a0505903e294a19c8e918ede5c8a2ee1
Brian Randall <brian.lewis.randall@outlook.com>
2019-11-13 01:05:58 +0000

    Flixter

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,14 @@ class Course < ApplicationRecord
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
   mount_uploader :image, ImageUploader   # Added for image uploader
+  belongs_to :course 
+
+
+  #c = Course.new
+  #c.image = params[:file]
 end
+
+#c.save!
+#c.image.url 
+#c.image.current_path
+#c.image_identifier
